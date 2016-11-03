20bf86e0d268256ef31d3a7632a1294ba7cabc8d
Ebba <Ebba@172.24.28.192>
2016-11-03 17:45:30 -0400

    added to index

--- cms270-project/src/Player.java
+++ cms270-project/src/Player.java
@@ -1,8 +1,43 @@
 
 public class Player {
-
-	public Player() {
-		// TODO Auto-generated constructor stub
+	private String name = "";
+	private double bet;
+	private Hand hand;
+	private double totalMoney;
+	
+	
+	public Player(String name, Hand hand, double totalmoney){
+	//Player will have a unique name, a unique hand and an amount of money to play Blackjack.
+		this.name = name;
+		this.hand = hand;
+		this.totalmoney = totalmoney;
 	}
+	public void startGame(Table table){
+	//Start a game of blackjack with a table.	
+	}
+	public void setBet(double bet){
+		this.bet = bet;
+	}
+	public void push(){
+	//When player and dealer have the score in hands.	
+	}
+	public void hit(){
+	//When player wants another card from the deck.
+	}
+	public void stand(){
+	//When player no longer wants another card from the deck.
+		
+	}
+	public void doubleDown(double bet){
+	//When player wants to double their bet, a
+	//and can no longer hit in current round of the game.
+	
+		bet = bet * 2;
+		
+	}
+	public void quit(){
+	//If player desires the quit the game.
+	}
+	
 
 }
