f614be3632b4cefb5e99f679952e086d0a8ba533
Ebba <Ebba@172.24.28.192>
2016-11-03 17:05:00 -0400

    Added totalPot instance variable, constructor, and checkPot, addPot and subPot methods. This class might be finished

--- cms270-project/src/Pot.java
+++ cms270-project/src/Pot.java
@@ -1,4 +1,21 @@
 
 public class Pot {
-
+	
+	private double totalPot;
+	
+	public Pot(){
+		totalPot = 0.0;
+	}
+	
+	public double checkPot(){
+		return totalPot;
+	}
+	
+	public void addPot(double bet){
+		totalPot+=bet;
+	}
+	
+	public void subPot(double amount){
+		totalPot-=amount;
+	}
 }
