65b3141418293e94fed08dc0c171285e6e830dfb
Carmen Cheng <Carmen Cheng @172.24.4.27>
2016-11-03 22:18:37 -0400

    Added suit parameter to Card; Deck constructor and shuffle method created

--- cms270-project/src/Card.java
+++ cms270-project/src/Card.java
@@ -1,15 +1,20 @@
+
 public class Card {
 
 	private int cardValue;
 	private char cardName;
+	private String suit;
 	
-	public Card(int value, char name){
+	public Card(int value, char name, String suit){
 		this.cardValue = value;
 		this.cardName = name;
+		this.suit = suit;
 	}
 	
 	public int getCardValue() {
 		return cardValue;
 	}
+	
+	public String 
 	
 }
--- cms270-project/src/Deck.java
+++ cms270-project/src/Deck.java
@@ -1,11 +1,47 @@
 import java.util.*;
 
-
 public class Deck {
+	private char name;
+	private int value;
+	private String suit;
+	private ArrayList<Card> deck;
+	int final DECK_SIZE = 52;
+	private int cardsUsed;
 
 	public Deck() {
-		ArrayList<Card> deck = new ArrayList<Card>(52);
+		deck = new ArrayList<Card>(DECK_SIZE);
+		String cardSuits [] = {"Spade", "Clover", "Heart", "Diamond"};
+		char cardNames [] = {'A', '2', '3', '4', '5', '6', '7', '8', '9,' '10'};
 		
+		for (int i = 0; i < cardSuits.length; i++) {
+			for (String s : cardSuits) {
+				int v = 1;
+				for (char n : cardNames) {
+					deck.add(new Card(v, n, s));
+					v++;
+				}
+			}
+		}
+		cardsUsed = 0;
 	}
-
+	
+	// Used cards are placed back into the deck and then reassigned
+	// to a random index. Card in random index replaces original card.
+	public void shuffle() {
+		// Used cards are placed back into the deck
+		for (int i = 0; i < 52; i++) {
+			Random rand = new Random();
+			int n = rand.nextInt(52);
+			Card temp = deck.get(i);
+			deck.set(i, deck.get(n));
+			deck.set(n, temp);
+		}
+		cardsUsed = 0;
+	}
+	
+	// Function returns the number of cards left
+	public int cardsLeft() {
+		return DECK_SIZE - cardsUsed;
+	}
+	
 }
