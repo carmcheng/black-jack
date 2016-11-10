345584c562e57ec3f97756b6113b92b55150f046
Carmen Cheng <Carmen Cheng @172.24.4.27>
2016-11-10 16:36:49 -0500

    Updated Deck class for ArrayList

--- cms270-project/src/Deck.java
+++ cms270-project/src/Deck.java
@@ -2,32 +2,32 @@ import java.util.*;
 
 public class Deck {
 	private ArrayList<Card> deck;
-	private final int SIZE_OF_DECK = 54;
+	private String cardSuits[] = {"Spade", "Clover", "Heart", "Diamond"};
+	private String cardNames[] = {"2", "3", "4", "5", "6", "7",
+								  "8", "9", "10", "J", "Q", "K", "A"};
 
 	public Deck() {
-		deck = new ArrayList<Card>(SIZE_OF_DECK);
-		String cardSuits[] = {"Spade", "Clover", "Heart", "Diamond"};
-		String cardNames[] = {"2", "3", "4", "5", "6", "7",
-							  "8", "9", "10", "J", "Q", "K", "A"};
-							  
-
-		for (String suit : cardSuits) {
-			int value = 1;
-			for (char name : cardNames) {
-				if (name == 'J' || name == 'Q' || name == 'K') {
-					value = 10;
-				} else if (name == 'A') {
-					// Set initial value of Ace to 11
-					value = 11;
-				} else {
-					value++;
+		deck = new ArrayList<Card>();
+		
+		for(int i = 0; i < 3; i++) {
+			for (String suit : cardSuits) {
+				int value = 1;
+				for (String name : cardNames) {
+					if (name.equals("J") || name.equals("Q") || 
+							name.equals("K") || name.equals("10")) {
+						value = 10;
+					} else if (name.equals("A")) {
+						// Set initial value of Ace to 11
+						value = 11;
+					} else {
+						value++;
+					}
+					deck.add(new Card(value, name, suit));
 				}
-				Card c = new Card(value, name, suit);
-				deck.add(c);
 			}
 		}
 	}
-	
+
 
 	// Used cards are placed back into the deck and then reassigned
 	// to a random index. Card in random index replaces original card.
@@ -36,23 +36,24 @@ public class Deck {
 			Random rand = new Random();
 			int n = rand.nextInt(52);
 			Card temp = deck.get(i);
-			deck.get(i) = deck.get(n);
-			deck[n] = temp;
+			deck.set(i, deck.get(n));
+			deck.set(n, temp);
 		}
-		cardsUsed = 0;
 	}
 
 	public Card dealCard() {
-		if (cardsUsed == 52) {
+		if (deck.size() == 52) {
 			shuffle();
 		}
-		cardsUsed++;
-		return deck[cardsUsed - 1];
+		int i = -1;
+		deck.remove(++i);
+
+		return deck.get(i);
 	}
 
 	// Function returns the number of cards left
 	public int cardsLeft() {
-		return DECK_SIZE - cardsUsed;
+		return deck.size();
 	}
 
 }
