32176c1bb71aede8909be4f464dfd58f97a1146a
Carmen Cheng <Carmen Cheng @172.24.4.27>
2016-11-13 23:57:46 -0500

    Fixed double down flag; tested ace value setting method but doesn't work

--- cms270-project/src/Table.java
+++ cms270-project/src/Table.java
@@ -9,10 +9,10 @@ public class Table {
 	private Player player;
 	private Pot pot;
 	private final int MAX_PLAYERS = 6;
-	
+
 	private Scanner scan = new Scanner(System.in);
 	private Iterator playerIterator;
-	
+
 	public Table(){
 		dealer = Dealer.getInstance();
 		numPlayers = 0;
@@ -48,17 +48,17 @@ public class Table {
 		int count = 1;
 
 		do {
-		System.out.println("Enter player name."); 
-		String name = scan.next();
-		System.out.println("How much money do you have?");
-		double money = scan.nextDouble();
-		player = new Player(name, money);
-		players.add(player);
-		numPlayers++;
-		count++;
+			System.out.println("Enter player name."); 
+			String name = scan.next();
+			System.out.println("How much money do you have?");
+			double money = scan.nextDouble();
+			player = new Player(name, money);
+			players.add(player);
+			numPlayers++;
+			count++;
 		} while (count <= number);
 	}
-	
+
 	public void firstDeal() {
 		playerIterator = new PlayerIterator(players);
 		while(playerIterator.hasNext()) {
@@ -67,43 +67,79 @@ public class Table {
 			currentPlayer.getHand().addCard(cardDeck.dealCard()); //adds two cards to player's hand on first deal
 			System.out.println(currentPlayer.getName() + "'s hand: ");
 			currentPlayer.printHand();
-			if(currentPlayer.getHand().checkHandValue() == 21) {
-				System.out.println("You have 21!"); // if they have 21
-				break;
+			if (currentPlayer.getHand().checkBlackjack()) {
+				System.out.println("You got blackjack!");
 			}
 		}
 		dealer.getHand().addCard(cardDeck.dealCard());
 		dealer.getHand().addCard(cardDeck.dealCard());
+		if(dealer.getHand().checkBlackjack()) {
+			System.out.println("Dealer got blackjack.");
+			return;
+		}
 		System.out.println("Dealer's hand: ");
 		System.out.println("\t" + dealer.getHand().getCards().get(0) + "\n\tHidden Card");
+	}
+	
+	public void dealerTurn() {
+		// Dealer conditions go here, soft17 and hard17 mechanisms
 	}
 
 	public void startRound() {
 		playerIterator = new PlayerIterator(players);
 		while(playerIterator.hasNext()) {
 			Player currentPlayer = (Player) playerIterator.next();
-			System.out.println("\nIt's your turn, " + currentPlayer.getName() + ".");
-			
-			if(currentPlayer.getHand().checkBlackjack()) { // if they have blackjack (changes state)
-				System.out.println("You got blackjack!");
+			if (currentPlayer.getHand().checkBlackjack() && playerIterator.hasNext()) {
 				currentPlayer = (Player) playerIterator.next();
-			} else {
-				System.out.println("Would you like to double down? Yes/No?");
-				String answer = scan.next();
-				while (!answer.equalsIgnoreCase("yes") && !answer.equalsIgnoreCase("no")) {
-					if(answer.equalsIgnoreCase("Yes")) {		// make sure player has enough money to double down
-						currentPlayer.doubleDown();
-					} else if (answer.equalsIgnoreCase("No")) {
-						System.out.println("You have chosen NOT to double down");
-					} else {
-						System.out.println("Enter a valid response.");
-						answer = scan.next();
-					}
-				}
+			}
+			System.out.println("\nIt's your turn, " + currentPlayer.getName() + ".");
 
-				System.out.println("Choose to hit or stand.");
+			// Double down option
+			System.out.println("Would you like to double down? Yes/No?");
+			String answer = scan.next();
+
+			while (!answer.equalsIgnoreCase("yes") && !answer.equalsIgnoreCase("no")) {
+				System.out.println("Enter a valid response.");
+				answer = scan.next();
+			}
+
+			boolean flag = false;
+			// Did player double down
+			if(answer.equalsIgnoreCase("Yes")) {		// make sure player has enough money to double down
+				currentPlayer.doubleDown();
+				currentPlayer.getHand().addCard(cardDeck.dealCard());
+				System.out.println("Your hand: ");
+				currentPlayer.printHand();
+				flag = true;
+				// Duplicate code; any way of encapsulating this in a method?
+				if(currentPlayer.getHand().checkHandValue() == 21) {
+					System.out.println("You have 21!"); // if they have 21
+					break;
+				} else if(currentPlayer.getHand().checkHandValue() > 21) {
+					System.out.println("You've busted."); // if they bust
+					break;
+				}
+			} else if (answer.equalsIgnoreCase("No")) {
+				System.out.println("You have chosen NOT to double down");
+			} 
+
+			// Player did not double down and gets to choose hit or stand
+			if (!flag) {
+				System.out.println("\nChoose to hit or stand.");
 				String move = scan.next();
-				while(move.equalsIgnoreCase("hit")) {
+				while (move.equalsIgnoreCase("hit")) {
+					// Trying to test Ace value.... setter method won't work?
+//					Card c = new Card(11, "A", "Test");
+//					currentPlayer.getHand().addCard(c);
+//					if (c.getCardName().equals("A")) {
+//						System.out.println("You got an Ace card. Value = 1 or 11?");
+//						int ans = scan.nextInt();
+//						if (ans == 1) {
+//							c.setCardValue(1);
+//						} else {
+//							break;
+//						}
+//					}
 					currentPlayer.getHand().addCard(cardDeck.dealCard());
 					System.out.println("Your hand:");
 					currentPlayer.printHand();
@@ -116,14 +152,15 @@ public class Table {
 					}
 					System.out.println("Hit or stand?");
 					move = scan.next();
-				} 
+				}
 			}
 		}
+		dealerTurn();
 	}
-	
+
 	public void playGame() {
 		playerIterator = new PlayerIterator(players);
-		
+
 		// Each player sets their bet
 		while(playerIterator.hasNext()) {
 			Player currentPlayer = (Player) playerIterator.next();
@@ -136,12 +173,12 @@ public class Table {
 			}
 			currentPlayer.setBet(bet);
 		}
-		
+
 		// Starting hands are dealt
 		firstDeal();
 		startRound();
 	}
-	
+
 	public static void main(String[] args){
 		Table table = new Table();
 		table.startGame();
