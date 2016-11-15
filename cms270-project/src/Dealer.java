aedcb13c6bcc4857ace074b19f3fb8851d5eb039
Ebba <Ebba@172.24.26.134>
2016-11-14 23:41:20 -0500

    change

--- cms270-project/src/Dealer.java
+++ cms270-project/src/Dealer.java
@@ -1,56 +1,27 @@
 
 public class Dealer {
 	
+	
 	private static Dealer dealer;
-	private Hand hand;
 	
 	/**
 	 * Private constructor to insure that only one Dealer object is made
 	 */
-	private Dealer() {
-		hand = new Hand(); 
-	}
+	private Dealer() {}
 	
 	/**
 	 * Method creates new Dealer object if not already initialized
 	 * If Dealer object does already exist, it returns the already initialized Dealer object
-	 * @return 
+	 * 
+	 * @return the one instance of the Dealer object
 	 */
 	public static Dealer getInstance() {
 		if(dealer == null) {
 			dealer = new Dealer(); 
-		} 
-		return dealer;
-	
-	}
-	
-	public boolean checkSoftSeventeen() {
-		if(hand.numOfCards() == 2 && hand.checkHandValue() == 17 &&
-				(hand.getCards().get(0).getCardName().equals("A") ||
-						hand.getCards().get(1).getCardName().equals("A"))) {
-			return true;
 		} else {
-			return false;
+			return dealer;
 		}
+		return dealer;
 	}
-	
 
-	public void addCard() {
-		
-	}
-	public void makeMove() {
-		
-	}
-	public Hand getHand() {
-		return hand;
-	}
-	
-	public void printHand() {
-		Iterator handIterator = new HandIterator(hand.getCards());
-		while(handIterator.hasNext()) {
-			Card card = (Card) handIterator.next();
-			System.out.println(card);
-		}
-	}
-	
 }
