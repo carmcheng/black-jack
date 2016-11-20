fda2be6efaf7e60bf45144f20b3d44594c66f843
Ebba <Ebba@172.24.17.166>
2016-11-20 15:55:55 -0500

    Added Application

--- cms270-project/src/BlackjackGUI.java
+++ cms270-project/src/BlackjackGUI.java
@@ -1,8 +1,16 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class BlackjackGUI {
+public class BlackjackGUI extends Application{
 
 	public BlackjackGUI() {
 		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
