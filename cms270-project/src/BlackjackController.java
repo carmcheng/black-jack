58434c56284658d8aab98c285421a532c52c9848
czoekim <czoekim@172.24.19.1>
2016-11-28 16:39:53 -0500

    Created basics needed for fxml gui building

--- cms270-project/src/BlackjackController.java
+++ cms270-project/src/BlackjackController.java
@@ -1,4 +1,16 @@
 
-public class BlackjackController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
 
+public class BlackjackController {
+	@FXML private TextField textInput;
+	@FXML private Label bottomLabel;
+	@FXML private Button rightButton;
+
+	@FXML protected void rightClick(ActionEvent event) {
+			String i = textInput.getText();
+			bottomLabel.setText(i);
+		
+	} 
 }
---
+++ cms270-project/src/BlackjackLauncher.java
@@ -0,0 +1,30 @@
+
+import java.net.URL;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class BlackjackLauncher extends Application {
+	public void start(Stage stage) {
+		try {
+			URL url = getClass().getResource("Blackjack_borderpane.fxml");
+			BorderPane root = FXMLLoader.load(url);
+			
+			Scene scene = new Scene(root, 600, 400);
+			
+			stage.setScene(scene);
+			stage.setTitle("Blackjack Game");
+			stage.show();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String [] args) {
+		launch(args);
+	}
+}
