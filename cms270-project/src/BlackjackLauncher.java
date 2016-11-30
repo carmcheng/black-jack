178f205c1be8ccb3f889b6b9b6d96fc14aff2e22
czoekim <czoekim@172.24.19.1>
2016-11-30 14:37:52 -0500

    Recreated basics to start FXML

--- cms270-project/src/BlackjackLauncher.java
+++ cms270-project/src/BlackjackLauncher.java
@@ -10,11 +10,14 @@ import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.scene.paint.Color;
+
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonType;
@@ -24,8 +27,12 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.TextInputDialog;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-
-
+import java.net.URL;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 /** This is our gui class
  */
 public class BlackjackLauncher extends Application{
@@ -33,80 +40,74 @@ public class BlackjackLauncher extends Application{
 	Label rootLabel;
 	TextField rootTF;
 	
-	protected void launchConfirmDialog() {
-		//confirmation dialog
-		Alert alert = new Alert(AlertType.CONFIRMATION);
-		alert.setTitle("Ready To Play Dialog");
-		alert.setHeaderText("Play!");
-		alert.setContentText("Are you ready to start the game?");
-
-		
-		//ERROR CHECK LATER: does not prevent user from playing the game
-		Optional<ButtonType> result = alert.showAndWait();
-		if (result.get() == ButtonType.OK){
-			rootLabel.setText("User is ready to play!");
-		} else {
-			rootLabel.setText("User DOESN'T want to start the game.");
+//	protected void launchConfirmDialog() {
+//		//confirmation dialog
+//		Alert alert = new Alert(AlertType.CONFIRMATION);
+//		alert.setTitle("Ready To Play Dialog");
+//		alert.setHeaderText("Play!");
+//		alert.setContentText("Are you ready to start the game?");
+//
+//		
+//		//ERROR CHECK LATER: does not prevent user from playing the game
+//		Optional<ButtonType> result = alert.showAndWait();
+//		if (result.get() == ButtonType.OK){
+//			rootLabel.setText("User is ready to play!");
+//		} else {
+//			rootLabel.setText("User DOESN'T want to start the game.");
+//		}
+//	}
+//	
+//	public int retrieveNumPlayers() {
+//		List<String> choices = new ArrayList<>();
+//		choices.add("1");
+//		choices.add("2");
+//		choices.add("3");
+//		choices.add("4");
+//		choices.add("5");
+//		choices.add("6");
+//
+//		ChoiceDialog<String> dialog = new ChoiceDialog<>("1", choices);
+//		dialog.setTitle("New Players");
+//		dialog.setHeaderText("Number of Players");
+//		dialog.setContentText("Choose how many players will play tonight:");
+//
+//		// Traditional way to get the response value.
+//		Optional<String> result = dialog.showAndWait();
+//		if (result.isPresent()){
+//			rootLabel.setText(result.get() + " players in this game\n");
+//			return Integer.parseInt(result.get());
+//		} else {
+//			launchErrorDialog("number of players");
+//			return 1; //default of 1 player
+//		}	
+//	}
+//	
+//	public void launchErrorDialog(String error) {
+//		Alert alert = new Alert(AlertType.ERROR);
+//		alert.setTitle("Error Dialog");
+//		alert.setHeaderText(error);
+//		alert.setContentText("Please enter a valid value for " + error);
+//		alert.showAndWait();
+//	}
+	
+	public void start(Stage stage) {
+		try {
+			URL url = getClass().getResource("Blackjack_borderpane.fxml");
+			BorderPane root = FXMLLoader.load(url);
+			
+			Scene scene = new Scene(root, 600, 400);
+			
+			stage.setScene(scene);
+			stage.setTitle("Example FXML GUI");
+			stage.show();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 	
-	public int retrieveNumPlayers() {
-		List<String> choices = new ArrayList<>();
-		choices.add("1");
-		choices.add("2");
-		choices.add("3");
-		choices.add("4");
-		choices.add("5");
-		choices.add("6");
-
-		ChoiceDialog<String> dialog = new ChoiceDialog<>("1", choices);
-		dialog.setTitle("New Players");
-		dialog.setHeaderText("Number of Players");
-		dialog.setContentText("Choose how many players will play tonight:");
-
-		// Traditional way to get the response value.
-		Optional<String> result = dialog.showAndWait();
-		if (result.isPresent()){
-			rootLabel.setText(result.get() + " players in this game\n");
-			return Integer.parseInt(result.get());
-		} else {
-			launchErrorDialog("number of players");
-			return 1; //default of 1 player
-		}	
-	}
-	
-	public void launchErrorDialog(String error) {
-		Alert alert = new Alert(AlertType.ERROR);
-		alert.setTitle("Error Dialog");
-		alert.setHeaderText(error);
-		alert.setContentText("Please enter a valid value for " + error);
-		alert.showAndWait();
-	}
-	
-	public static void main (String [] args) {
-		Application.launch(args);
-	}
-	
-	@Override
-	
-	public void start(Stage stage) {
-		stage.setTitle("Prepare to lose all your money");
-		BorderPane bp = new BlackjackController();
-		rootLabel = new Label("Results of our dialog will go here");
-		rootTF = new TextField();
-		bp.getChildren().addAll(rootLabel, rootTF);
-		
-		Scene scene = new Scene(bp, 600, 400, Color.DARKGREEN);
-		
-		
-	//	bp.setPadding(new Insets(10, 20, 10, 20));
-		
-		stage.setScene(scene);
-		stage.show();
-		
-		
-		launchConfirmDialog();
-		
+	public static void main(String [] args) {
+		launch(args);
 	}
 	
 
